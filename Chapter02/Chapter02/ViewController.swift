1b7bb90a99c0dd057c0c6d4c518b93ecf6bdc2e2
Frank.zhang <Frank.zhang@neulion.com.cn>
2019-08-05 15:58:44 +0800

    Add  Chapter02

---
+++ Chapter02/Chapter02/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Chapter02
+//
+//  Created by frank.zhang on 2019/7/31.
+//  Copyright © 2019 Frank.zhang. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Set the view's delegate
+        sceneView.delegate = self
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "ARResource.scnassets/SceneKit Scene.scn")!
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    // MARK: - ARSCNViewDelegate
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
